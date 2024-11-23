3d87fae147b66ae679a66dfdc0c5a3023669ef6d
Mohit13333 <mohitkumar143680@gmail.com>
2024-11-24 00:14:10 +0530

    loading state

--- Excellence/src/store/auth.jsx
+++ Excellence/src/store/auth.jsx
@@ -92,8 +92,9 @@ const AuthProvider = ({ children }) => {
   // }, []);
   if (isLoading) {
     return (
-      <section className="flex items-center justify-center h-screen">
+      <section className="flex flex-col items-center justify-center h-screen">
         <div className="w-[100px] h-[100px] border-8 border-t-8 border-r-blue-600 border-t-green-600 border-l-rose-600 border-solid rounded-full animate-spin"></div>
+        <p className="text-3xl mt-3 text-white">Please wait while we fetch all your data...</p>
       </section>
     );
   }
